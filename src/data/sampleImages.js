193b3711149854f927b62bd72438515db6cfc994
Ababii Vicu <vicu-marin.ababii@student.upt.ro>
2025-06-05 07:54:03 +0300

    images changed

--- src/data/sampleImages.js
+++ src/data/sampleImages.js
@@ -4,16 +4,13 @@
 // For now we’re using Picsum placeholders. Total of 10 entries.
 
 const sampleImages = [
-    "https://picsum.photos/id/1010/500/500",
-    "https://picsum.photos/id/1011/500/500",
-    "https://picsum.photos/id/1012/500/500",
-    "https://picsum.photos/id/1013/500/500",
-    "https://picsum.photos/id/1015/500/500",
-    "https://picsum.photos/id/1020/500/500",
-    "https://picsum.photos/id/1024/500/500",
-    "https://picsum.photos/id/1027/500/500",
-    "https://picsum.photos/id/1033/500/500",
-    "https://picsum.photos/id/1035/500/500",
+    "https://static.zara.net/assets/public/4d74/b35f/7b684e42b629/c9a5393ec25a/00039696406-p/00039696406-p.jpg?ts=1747066070228&w=386",
+    "https://static.zara.net/assets/public/89d0/049f/d673444482f0/1479e6e5fa3e/03854688914-ult3/03854688914-ult3.jpg?ts=1747902590389&w=386",
+    "https://static.zara.net/assets/public/743c/173d/da0046b68725/68558535030e/01213648800-300-ult1/01213648800-300-ult1.jpg?ts=1724246496071&w=386",
+    "https://static.zara.net/assets/public/9cba/4583/5abc4b5f80bf/9510c14c1faf/02675623630-p/02675623630-p.jpg?ts=1748341783611&w=1024",
+    "https://static.zara.net/assets/public/16bf/b6cf/dc97498894ca/b3d47467d9f9/01394481500-p/01394481500-p.jpg?ts=1742977870194&w=386",
+    "https://static.zara.net/assets/public/d15f/2bb6/994941118243/bac0a6c4b281/07251601250-p/07251601250-p.jpg?ts=1742977872390&w=386",
+    "https://static.zara.net/assets/public/a4d2/6221/6cf04f93a4bb/4e8c30311f9f/01639640527100-p/01639640527100-p.jpg?ts=1741794987066&w=386",
 ];
 
 export default sampleImages;
